b07b0ef4e8effeba430cba383d83bbaa230127f6
Satoshi Ogata <satosystems@gmail.com>
2017-11-14 18:07:50 +0900

    use ByteString instead of String
    約 23.3MiB/s が出るようになった。
    
    ```
    $ .stack-work/dist/x86_64-osx/Cabal-1.24.2.0/build/yes/yes | pv -r > /dev/null
    [23.3MiB/s]
    ```

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import qualified Data.ByteString.Char8 as BC8
+
 
 main :: IO ()
 main = loop
  where
-  loop = putStrLn "y" >> loop
+  loop = BC8.putStrLn "y" >> loop
 
